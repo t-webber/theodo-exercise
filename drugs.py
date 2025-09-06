c2c5e103a1d2d763dba2701ad84d9b5fb427108d
Tom Webber <t.c.w.webber@gmail.com>
2025-09-06 09:44:10 +0200

    ref(drug-entries): add the different drugs and their logic

---
+++ drugs.py
@@ -0,0 +1,43 @@
+from drug import Drug
+
+
+class InsulineVial(Drug):
+
+    def __init__(self, days_until_expiration: int, efficiency_percentage: int) -> None:
+        super().__init__("Insulin vial", days_until_expiration, efficiency_percentage)
+
+    def _degrade_efficiency_unexpired(self):
+        if self.days_until_expiration <= 0:
+            self.efficiency_percentage = 0
+        if self.days_until_expiration  <= 7:
+            self.efficiency_percentage -= 3
+        elif self.days_until_expiration <= 30:
+            self.efficiency_percentage -= 2
+        else:
+            self.efficiency_percentage -= 1
+
+
+class GrannyRecipe(Drug):
+
+    def __init__(self, days_until_expiration: int, efficiency_percentage: int) -> None:
+        super().__init__("Granny recipe", days_until_expiration, efficiency_percentage)
+
+    def _degrade_efficiency_unexpired(self):
+        pass
+
+    def _update_expiration(self) -> None:
+        pass
+
+
+
+class BottleOfWine(Drug):
+
+    def __init__(self, days_until_expiration: int, efficiency_percentage: int) -> None:
+        super().__init__("Old bottle of wine", days_until_expiration, efficiency_percentage)
+
+
+    def _degrade_efficiency_unexpired(self):
+        self.efficiency_percentage -= 1
+
+
+
