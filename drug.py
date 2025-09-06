afcf93d37aca477515d438498fffe855475b4fec
Tom Webber <t.c.w.webber@gmail.com>
2025-09-06 09:42:41 +0200

    ref(drug): add new drug class with better state handling
    Move drug update logic inside the drug class

---
+++ drug.py
@@ -0,0 +1,33 @@
+class Drug:
+
+    def __init__(self, name: str, days_until_expiration: int, efficiency_percentage: int) -> None:
+        self.name: str = name
+        self.days_until_expiration: int = days_until_expiration
+        self.efficiency_percentage: int = efficiency_percentage
+
+    def __repr__(self):
+        return f"{self.name}, {self.days_until_expiration}, {self.efficiency_percentage}"
+
+    def _degrade_efficiency_unexpired(self):
+        """
+        Degrades the efficiency_percentage corresponding to a day when the drug is unexpired
+        """
+        self.efficiency_percentage -= 1
+
+
+    def _update_expiration(self) -> None:
+        """
+        Updates the number of days until the expiration date
+        """
+        self.days_until_expiration -= 1
+
+    def update_efficiency_percentage(self) -> None:
+        """
+        Updates the efficiency_percentage of a self after one more more.
+        """
+        self._update_expiration()
+        self._degrade_efficiency_unexpired()
+        if self.days_until_expiration < 0:
+            self._degrade_efficiency_unexpired()
+
+
